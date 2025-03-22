98a4fdae3adb402b88cde3cd81e8324d8d95a063
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-03-22 19:31:24 +0500

    Refactor layout and spacing in contactus component for improved responsiveness. First draft responsive done.

--- src/lib/home/contactus.svelte
+++ src/lib/home/contactus.svelte
@@ -5,17 +5,17 @@
         " inline-block bg-[linear-gradient(to_right,#ffffff80_0%,#ffffff_15%,#ffffff_85%,#ffffff80_100%)] bg-clip-text text-transparent ";
 </script>
 
-<div class="mx-32 mb-24 px-6 lg:px-0 lg:max-w-[1700px]">
+<div class="mx-6 mb-10 md:mx-10 lg:mx-20 xl:mx-32 xl:mb-24 xl:px-6 xl:px-0 xl:max-w-[1700px]">
     <div class="border-2 rounded-[20px] border-bw500/70">
         <div class="container">
             <img
                 src={coverSvg}
                 alt="Background image"
-                class="background-image"
+                class="h-full w-full object-cover top-0 left-0 absolute"
             />
             <div class="text-overlay">
                 <p class="small-text">Book a meeting</p>
-                <p class={"large-text" + gradienttext}>
+                <p class={"text-[30px] lg:text-[40px]" + gradienttext}>
                     Speak with our dedicated sales team
                 </p>
             </div>
@@ -32,16 +32,6 @@
         border-radius: 20px;
         overflow: hidden;
     }
-
-    .background-image {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-
     .text-overlay {
         position: absolute;
         top: 30%;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,17 +18,17 @@
     });
 </script>
 
-<!-- <div class="bg-pink-500 sm:bg-red-500 md:bg-blue-500 lg:bg-green-500 xl:bg-yellow-500">  -->
+<!-- <div class="bg-pink-800 sm:bg-red-800 md:bg-blue-800 lg:bg-green-800 xl:bg-yellow-800">  -->
 <div>
     <Hero />
-    <!-- <TrustedBy />
+    <TrustedBy />
     <MultiLanguage />
     <Observability />
     <Workitemqueue />
-    <Reports /> -->
+    <Reports />
     <Security />
-    <!-- <Increased />
+    <Increased />
     <Testimonials />
     <Joincommunity />
-    <Contactus /> -->
+    <Contactus />
 </div>
