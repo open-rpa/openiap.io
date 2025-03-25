202890605e7088afad6d120031179be56ef83a2a
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-03-26 00:16:11 +0500

    Fixed pricings

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -8,6 +8,7 @@
   import Separator from "$lib/components/ui/separator/separator.svelte";
   import border_bottom from "$lib/images/border_bottom.svg";
   import border_bottom_large from "$lib/images/border_bottom_large.svg";
+  import pricing_grid from "$lib/images/pricing_grid.png";
 
   let screen = $state(1);
   $effect(() => {
@@ -32,10 +33,19 @@
 
   let gradienttext =
     " inline-block bg-[linear-gradient(to_right,#ffffff80_0%,#ffffff_15%,#ffffff_85%,#ffffff80_100%)] bg-clip-text text-transparent ";
+  let gradienttextnew =
+    " inline-block bg-[linear-gradient(to_right,#ffffff80_0%,#ffffff_15%,#ffffff_85%,#ffffff80_100%)] bg-clip-text text-transparent ";
 </script>
 
-<section class="pt-20 md:min-h-screen px-4 gradient-bg">
-  <div class="container mx-auto">
+<section
+  class="pt-32 md:h-screen px-4 gradient-bg flex justify-center items-center md:mb-48"
+>
+  <img
+    src={pricing_grid}
+    alt="pricing_grid"
+    class="hidden md:block md:absolute w-[100%] h-[100%] object-cover top-0 left-0 z-0"
+  />
+  <div class="container mx-auto md:absolute md:h-screen">
     <div class="text-center">
       <div class="mb-10">
         <div class="text-center">
@@ -49,7 +59,7 @@
       </div>
     </div>
 
-    <Tabs.Root value="3">
+    <Tabs.Root value="1">
       <div class="flex justify-center mb-10">
         <Tabs.List
           class="bg-bw700 dark:bg-bw700 rounded-[5px] p-1 lg:mb-0 text-bw50 font-normal flex justify-center w-fit rounded-[10px]"
@@ -66,19 +76,23 @@
         </Tabs.List>
       </div>
       <Tabs.Content value="1">
-        <div class="flex justify-center space-x-[40px] animate-fade-in">
+        <div
+          class="grid grid-cols-1 gap-8 md:flex md:justify-center animate-fade-in"
+        >
           <Card.Root
-            class="md:col-span-3 lg:col-start-2 lg:col-span-2  transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[510px] h-[600px] px-[26px] py-1.5 border-bw500/70"
+            class="md:col-span-3 lg:col-start-2 lg:col-span-2  transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-full lg:w-[510px] lg:h-[530px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header class="mb-0">
-              <Card.Title class="font-normal mb-2">Open Source</Card.Title>
+              <Card.Title class={"font-normal mb-2" + gradienttextnew}
+                >Open Source</Card.Title
+              >
               <Card.Description>Perfect for getting started</Card.Description>
-              <div class="pt-5 pb-[30px]">
+              <div class="pt-2 pb-[14px]">
                 <Separator />
               </div>
               <Card.Content class="p-0">
-                <div class="grid grid-cols-1 space-x-4">
-                  <div class="m-1 mb-10">
+                <div class="grid grid-cols-1">
+                  <div class="m-1 mb-5">
                     <p class="flex items-center my-2 text-bw50 text-[14px]">
                       Control and Scalability
                     </p>
@@ -122,12 +136,14 @@
           </Card.Root>
 
           <Card.Root
-            class="md:col-span-3 lg:col-start-4 lg:col-span-2 transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[510px] px-[26px] py-1.5 border-bw500/70"
+            class="md:col-span-3 lg:col-start-4 lg:col-span-2 transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-full lg:w-[510px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header>
-              <Card.Title class="font-normal mb-2">Enterprise</Card.Title>
+              <Card.Title class={"font-normal mb-2" + gradienttextnew}
+                >Enterprise</Card.Title
+              >
               <Card.Description>Customized solutions</Card.Description>
-              <div class="pt-5 pb-[30px]">
+              <div class="pt-2 pb-[14px]">
                 <Separator />
               </div>
               <Card.Content class="p-0">
@@ -152,7 +168,7 @@
                     </ul>
                   </div>
 
-                  <div class="m-1">
+                  <div class="m-1 lg:ms-12 justify-end">
                     <p class="flex items-center my-2 text-bw50 text-[14px]">
                       Scalability
                     </p>
@@ -211,20 +227,41 @@
         </div>
       </Tabs.Content>
       <Tabs.Content value="2">
-        <div class="flex justify-center space-x-4 animate-fade-in">
+        <div
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 md:gap-4 xl:gap-4 xl:flex xl:justify-center animate-fade-in"
+        >
           <Card.Root
-            class=" hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[335px] px-[26px] py-1.5 border-bw500/70"
+            class=" hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark xl:w-[335px] xl:h-[452px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header>
-              <Card.Title class="font-normal">Open Source</Card.Title>
+              <Card.Title class={"font-normal" + gradienttextnew}
+                >Open Source</Card.Title
+              >
               <Card.Description>Perfect for getting started</Card.Description>
             </Card.Header>
 
-            <div class="flex p-6 items-end text-bw50">
-              <span class="text-[30px] font-bold text-center">0 USD </span>
+            <div class="flex px-6 items-end text-bw50 my-6">
+              <span class="text-[30px] font-bold text-center">Free</span>
             </div>
 
-            <div class="pt-5 pb-[30px] px-6">
+            <!-- <div class="pt-5">
+                <div class="flex px-6 items-end text-bw50">
+                  <span class="text-[30px] font-bold text-center"
+                    >Free
+                    <span
+                      class="ms-1 font-bold text-center justify-end text-bw500 text-[14px]"
+                      >/Month</span
+                    >
+                  </span>
+                </div>
+                <div class="flex px-6 pb-6 items-end text-[14px]">
+                  <span class="font-bold text-center text-bw500"
+                    >Approx. 39 <span class="text-bw500">USD</span></span
+                  >
+                </div>
+              </div> -->
+
+            <div class="px-5">
               <img src={border_bottom} alt="border_bottom" />
             </div>
 
@@ -256,10 +293,12 @@
           </Card.Root>
 
           <Card.Root
-            class=" hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[335px] px-[26px] py-1.5 border-bw500/70"
+            class=" hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark xl:w-[335px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header>
-              <Card.Title class="font-normal">Cloud Basic</Card.Title>
+              <Card.Title class={"font-normal" + gradienttextnew}
+                >Cloud Basic</Card.Title
+              >
               <Card.Description>Perfect for Beginners</Card.Description>
             </Card.Header>
 
@@ -274,13 +313,13 @@
                 </span>
               </div>
               <div class="flex px-6 pb-6 items-end text-[14px]">
-                <span class="font-bold text-center text-bw500"
-                  >Approx. 39 <span class="text-bw500">USD</span></span
+                <span class="font-bold text-center text-bw200"
+                  >Approx. 39 <span class="text-bw200">USD</span></span
                 >
               </div>
             </div>
 
-            <div class="pt-2 pb-[30px] px-6">
+            <div class="px-6">
               <img src={border_bottom} alt="border_bottom" />
             </div>
 
@@ -313,10 +352,12 @@
           </Card.Root>
 
           <Card.Root
-            class="transition-all duration-300 hover:shadow-lg border-customGreen flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[335px] px-[26px] py-1.5 border-bw500/70"
+            class="transition-all duration-300 hover:shadow-lg border-customGreen flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark xl:w-[335px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header>
-              <Card.Title class="font-normal">Cloud Premium</Card.Title>
+              <Card.Title class={"font-normal" + gradienttextnew}
+                >Cloud Premium</Card.Title
+              >
               <Card.Description
                 >Perfect for medium-size projects</Card.Description
               >
@@ -333,13 +374,13 @@
                 </span>
               </div>
               <div class="flex px-6 pb-6 items-end text-[14px]">
-                <span class="font-bold text-center text-bw500"
-                  >Approx. 129 <span class="text-bw500">USD</span></span
+                <span class="font-bold text-center text-bw200"
+                  >Approx. 129 <span class="text-bw200">USD</span></span
                 >
               </div>
             </div>
 
-            <div class="pt-2 pb-[30px] px-6">
+            <div class="px-6">
               <img src={border_bottom} alt="border_bottom" />
             </div>
             <Card.Content>
@@ -371,11 +412,15 @@
           </Card.Root>
 
           <Card.Root
-            class=" hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[335px] px-[26px] py-1.5 border-bw500/70"
+            class=" hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark xl:w-[335px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header>
-              <Card.Title class="font-normal">Cloud Advanced</Card.Title>
-              <Card.Description>Perfect for advanced projects</Card.Description>
+              <Card.Title class={"font-normal" + gradienttextnew}
+                >Cloud Advanced</Card.Title
+              >
+              <Card.Description
+                >Perfect for advanced projects</Card.Description
+              >
             </Card.Header>
 
             <div class="pt-5">
@@ -389,13 +434,13 @@
                 </span>
               </div>
               <div class="flex px-6 pb-6 items-end text-[14px]">
-                <span class="font-bold text-center text-bw500"
-                  >Approx. 239 <span class="text-bw500">USD</span></span
+                <span class="font-bold text-center text-bw200"
+                  >Approx. 239 <span class="text-bw200">USD</span></span
                 >
               </div>
             </div>
 
-            <div class="pt-2 pb-[30px] px-6">
+            <div class="px-6">
               <img src={border_bottom} alt="border_bottom" />
             </div>
 
@@ -431,10 +476,12 @@
       <Tabs.Content value="3">
         <div class="flex justify-center animate-fade-in">
           <Card.Root
-            class="md:col-start-2 md:col-span-3 lg:md:col-span-1 lg:col-start-2 hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark w-[400px] px-[26px] py-1.5 border-bw500/70"
+            class="md:col-start-2 md:col-span-3 lg:md:col-span-1 lg:col-start-2 hover:transition-all duration-300 hover:shadow-lg flex flex-col justify-between rounded-[20px] border-2 bg-cardbgdark dark:bg-cardbgdark lg:w-[400px] px-[26px] py-1.5 border-bw500/70"
           >
             <Card.Header>
-              <Card.Title class="font-normal">Hourly</Card.Title>
+              <Card.Title class={"font-normal" + gradienttextnew}
+                >Hourly</Card.Title
+              >
               <Card.Description
                 >Perfect for onboarding developers</Card.Description
               >
@@ -446,18 +493,18 @@
                   >DKK 1530
                   <span
                     class="ms-1 font-bold text-center justify-end text-bw500 text-[14px]"
-                    >/Month</span
+                    >/Hour</span
                   >
                 </span>
               </div>
               <div class="flex px-6 pb-6 items-end text-[14px]">
-                <span class="font-bold text-center text-bw500"
-                  >Approx. 216 <span class="text-bw500">USD</span></span
+                <span class="font-bold text-center text-bw200"
+                  >Approx. 216 <span class="text-bw200">USD</span></span
                 >
               </div>
             </div>
 
-            <div class="pt-2 pb-[20px] px-6">
+            <div class="px-6">
               <img src={border_bottom_large} alt="border_bottom" />
             </div>
 
@@ -507,3 +554,5 @@
     </div>
   </div>
 </section>
+
+
