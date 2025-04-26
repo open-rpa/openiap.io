751090fec6e4241775a9460dc5b469d1885d0f92
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-04-26 17:43:08 +0200

    Add footer SVG image and update Footer.svelte to include it

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -5,6 +5,7 @@
 	import Logo1 from "$lib/images/footer/linkedin.svg";
 	import Logo2 from "$lib/images/footer/youtube.svg";
 	import Logo3 from "$lib/images/footer/git.svg";
+	import footerSvg from "$lib/images/footer/footer3.svg";
 
 	let isScreenSmall = $state();
 	let isScreenSmallTab = $state();
@@ -40,6 +41,7 @@
 <section
 	class="pt-10 pb-16 mx-6 md:mx-10 lg:mx-20 xl:mx-32 xl:flex xl:flex-col xl:justify-center"
 >
+	<img src={footerSvg} alt="Logo" loading="lazy" />
 	<div
 		class={`w-full lg:max-w-[1700px] ${isScreenSmall && "grid grid-cols-2 mt-4 space-y-2 text-center items-center "} ${isScreenSmallTab && "grid grid-cols-4 mt-4 space-y-2 text-center items-center "} ${isScreenMedium && "grid grid-cols-5 space-y-2 text-center items-center"}  ${isScreenLarge && "flex flex-row justify-between"} `}
 	>
